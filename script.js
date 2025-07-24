7dc31e8919db0b904622c6fb45536b5ee9a751e3
Zubair  Farooq <zubair.farooq@muztech.io>
2025-07-24 18:40:16 +0500

    Initial commit

---
+++ script.js
@@ -0,0 +1,20 @@
+const swiper = new Swiper(".mySwiper", {
+  slidesPerView: 3,
+  spaceBetween: 30,
+  loop: true,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  breakpoints: {
+    320: {
+      slidesPerView: 1,
+    },
+    768: {
+      slidesPerView: 2,
+    },
+    1024: {
+      slidesPerView: 3,
+    },
+  },
+});
