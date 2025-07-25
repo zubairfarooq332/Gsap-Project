812ee6e9b17513877dbf17d24809965f820217d1
Zubair Farooq <zubairfarooqmuztech@gmail.com>
2025-07-24 21:13:35 -0700

    animation and navmenu created

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
+const menu = document.getElementById("menu");
+const menuIcon = document.getElementById("menuIcon");
+const menuToggleIcon = document.getElementById("menuToggleIcon");
+const closeBtn = document.getElementById("closeBtn");
+
+let menuOpen = false;
+
+menuIcon.addEventListener("click", () => {
+  menu.classList.add("open");
+  menuToggleIcon.src = "images/close.png"; // cross icon
+  menuOpen = true;
+});
+
+closeBtn.addEventListener("click", () => {
+  menu.classList.remove("open");
+  menuToggleIcon.src = "images/menu.png"; // back to menu icon
+  menuOpen = false;
+});
+
 const swiper = new Swiper(".mySwiper", {
   slidesPerView: 3,
   spaceBetween: 30,
@@ -17,4 +36,32 @@ const swiper = new Swiper(".mySwiper", {
       slidesPerView: 3,
     },
   },
+});
+
+// Gsap
+gsap.from(".language", {
+  opacity: 0,
+  delay: 1,
+  duration: 1.5,
+  ease: "power2.out",
+});
+gsap.from(".logo", {
+  opacity: 0,
+  delay: 2,
+  duration: 1.5,
+  ease: "power2.out",
+});
+gsap.from(".menuBar", {
+  opacity: 0,
+  delay: 2.5,
+  duration: 1.5,
+  ease: "power2.out",
+});
+
+gsap.from("#text-overlay text", {
+  y: 100, // Move from bottom
+  opacity: 0, // Start hidden
+  duration: 1.5,
+  ease: "power3.out",
+  delay: 0.2, // Optional
 });
